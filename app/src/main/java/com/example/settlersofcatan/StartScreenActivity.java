00ce8a11946f11676a488aebbb23e9cfb055579c
Martin Unterhuber <m.unterhuber99@gmail.com>
2021-06-17 15:29:45 +0200

    Fixed crashes

--- app/src/main/java/com/example/settlersofcatan/StartScreenActivity.java
+++ app/src/main/java/com/example/settlersofcatan/StartScreenActivity.java
@@ -8,21 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class StartScreenActivity extends AppCompatActivity {
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
         Button sbtn = (Button)findViewById(R.id.startscreen_button);
-        sbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(StartScreenActivity.this,MainActivity.class));
-            }
+        sbtn.setOnClickListener(v -> {
+            startActivity(new Intent(this ,MainActivity.class));
+            this.finish();
         });
-
-
     }
 }
